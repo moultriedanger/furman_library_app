ccbb8e89f1738d5e8f0aff78d8dfff4d7a9590ca
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-04-27 15:17:26 -0400

    format for artist and collector forms

--- app/app.py
+++ app/app.py
@@ -1,19 +1,33 @@
 from flask import Flask, render_template
+import oracledb
 
 app = Flask(__name__)
+
+# Oracle DB connection config
+dsn = oracledb.makedsn('csdb.fu.campus', 1521, sid='cs40')
+connection = oracledb.connect(user='agardner', password='Agar1324', dsn=dsn)
 
 @app.route('/')
 def landing_page():
     return render_template('landing.html')
 
-#Artist Information Form Page
-@app.route('/artist')
-def artist_form():
+@app.route('/artist_info')
+def artist_info():
     return render_template('artist_info.html')
 
+# Route for Artist Form Page
+@app.route('/artist')
+def artist_form():
+    return render_template('artist_form.html')
+
 # Collector Information Form Page
-@app.route('/collector')
+@app.route('/collector_form')
 def collector_form():
+    return render_template('collector_form.html')
+
+# Collector Information Form Page
+@app.route('/collector_info')
+def collector_info():
     return render_template('collector_info.html')
 
 # Artwork Information Form Page
